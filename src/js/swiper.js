a0a827395b93b001937384d1965ab3155804bb9a
JuniorLevel <work.junior@mail.ru>
2023-05-17 13:48:18 +0300

    Адаптив секции popular

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,16 +3,26 @@ import Swiper, { Navigation } from "swiper";
 const swiper = new Swiper(".swiper", {
   // Default parameters
   modules: [Navigation],
-  autoplay: {
-    delay: 300,
-  },
-  direction: "horizontal",
-  slidesPerView: 3,
-  spaceBetween: 30,
+  //   autoplay: {
+  //     delay: 300,
+  //   },
+  slidesPerView: 1,
+  direction: "vertical",
+  spaceBetween: 0,
+  autoHeight: true,
   effect: "fade",
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+
+  //breakpoints
+  breakpoints: {
+    1075: {
+      direction: "horizontal",
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 });
 
